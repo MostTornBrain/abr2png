19bb574a554034e8e8a3d177259329b3b5ef123e
Brian Stormont <MostTornBrain@users.noreply.github.com>
2025-11-29 19:19:11 -0500

    Bump the rev

--- abr.cpp
+++ abr.cpp
@@ -71,7 +71,7 @@ int main(int argc, char* argv[])
 			<<"    abr2png - Photoshop Brush converter"<<endl
 			<<endl
 			<<"Version:"<<endl
-			<<"    abr2png v2.0.2 (29-nov-2025)"<<endl
+			<<"    abr2png v2.0.3 (29-nov-2025)"<<endl
 			<<endl
 			<<"Description:"<<endl
 			<<"    abr2png - tool to convert from Photoshop Brush (.abr) and"<<endl
